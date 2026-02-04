ff6d3c57c3f94c22a377e2887b75c0c82b04bfa5
Ashish Bedi <ashish@savyre.com>
2026-02-04 15:47:46 +0530

    counter app with style

--- script.js
+++ script.js
@@ -7,18 +7,58 @@
 // paragraph[0].innerHTML = "This is a changed paragraph.";
 
 
-const heading  = document.getElementById("heading");
-console.log(heading);
-heading.style.color = "blue";
-heading.innerHTML="heading chnage ker de mene"
+// const heading  = document.getElementById("heading");
+// console.log(heading);
+// heading.style.color = "blue";
+// heading.innerHTML="heading chnage ker de mene"
 
 
-const paragraph = document.getElementById("paragraph");
-paragraph.style.fontSize = "20px";
-paragraph.style.color = "green";
-paragraph.innerHTML="paragraph change ker de mene"
+// const paragraph = document.getElementById("paragraph");
+// paragraph.style.fontSize = "20px";
+// paragraph.style.color = "green";
+// paragraph.innerHTML="paragraph change ker de mene"
 
-const para =document.getElementsByClassName("para");
-para[0].style.fontSize="30px";
-para[0].style.color="red";
-para[0].innerHTML="testing class"
+// const para =document.getElementsByClassName("para");
+// para[0].style.fontSize="30px";
+// para[0].style.color="red";
+// para[0].innerHTML="testing class"
+
+
+// const btn1 = document.getElementById("btn");
+// btn1.addEventListener("click", () => {
+//     alert("Button 1 clicked!");
+// })
+
+
+
+
+// const btn1 = document.getElementById("btn");
+
+// btn1.addEventListener("click", () => {
+//       alert("Button 1 clicked!");
+// })
+
+
+
+const counter1 = document.getElementById("counter");
+const incrementBtn = document.getElementById("increment");
+const decrementBtn = document.getElementById("decrement");      
+const resetBtn = document.getElementById("reset");
+
+let count = 0;
+
+incrementBtn.addEventListener("click", () => {
+    count++; 
+    counter1.innerHTML = count;
+})
+
+decrementBtn.addEventListener("click", () => {
+    count--; 
+    counter1.innerHTML = count;
+})
+
+resetBtn.addEventListener("click", () => {
+    count = 0; 
+    counter1.innerHTML = count;
+})
+
