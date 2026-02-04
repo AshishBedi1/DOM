415ee5104f4b9e8872d0bd508fa8836e3fab3cba
Ashish Bedi <ashish@savyre.com>
2026-02-04 15:04:01 +0530

    Basic DOM

---
+++ script.js
@@ -0,0 +1,24 @@
+// const heading = document.getElementById("heading");
+// console.log(heading);
+// heading.innerHTML = "Hello World!";
+
+// const paragraph = document.getElementsByClassName("paragraph");
+// console.log(paragraph);
+// paragraph[0].innerHTML = "This is a changed paragraph.";
+
+
+const heading  = document.getElementById("heading");
+console.log(heading);
+heading.style.color = "blue";
+heading.innerHTML="heading chnage ker de mene"
+
+
+const paragraph = document.getElementById("paragraph");
+paragraph.style.fontSize = "20px";
+paragraph.style.color = "green";
+paragraph.innerHTML="paragraph change ker de mene"
+
+const para =document.getElementsByClassName("para");
+para[0].style.fontSize="30px";
+para[0].style.color="red";
+para[0].innerHTML="testing class"
