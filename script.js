ac041fc39c21fba3f5ff2bef59d0ed05e7f3af27
Ashish Bedi <ashish@savyre.com>
2026-02-05 15:24:43 +0530

    to-do list

--- script.js
+++ script.js
@@ -40,25 +40,55 @@
 
 
 
-const counter1 = document.getElementById("counter");
-const incrementBtn = document.getElementById("increment");
-const decrementBtn = document.getElementById("decrement");      
-const resetBtn = document.getElementById("reset");
+// const counter1 = document.getElementById("counter");
+// const incrementBtn = document.getElementById("increment");
+// const decrementBtn = document.getElementById("decrement");      
+// const resetBtn = document.getElementById("reset");
 
-let count = 0;
+// let count = 0;
 
-incrementBtn.addEventListener("click", () => {
-    count++; 
-    counter1.innerHTML = count;
-})
+// incrementBtn.addEventListener("click", () => {
+//     count++; 
+//     counter1.innerHTML = count;
+// })
 
-decrementBtn.addEventListener("click", () => {
-    count--; 
-    counter1.innerHTML = count;
-})
+// decrementBtn.addEventListener("click", () => {
+//     count--; 
+//     counter1.innerHTML = count;
+// })
 
-resetBtn.addEventListener("click", () => {
-    count = 0; 
-    counter1.innerHTML = count;
-})
+// resetBtn.addEventListener("click", () => {
+//     count = 0; 
+//     counter1.innerHTML = count;
+// })
+
+
+const input = document.getElementById("todoinput");
+const addBtn = document.getElementById("addBtn");
+const todoList = document.getElementById("todolist");
+
+addBtn.addEventListener("click", () => {
+    const todotext = input.value;
+
+    if (todotext === "") {
+        alert("Please enter a todo item.");
+        return;
+    }
+
+    const li = document.createElement("li");
+    li.innerText = todotext;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "Delete";
+
+    deleteBtn.addEventListener("click", () => {
+        li.remove();
+    });
+
+    li.appendChild(deleteBtn);
+    todoList.appendChild(li);
+
+    input.value = "";
+});
+
 
