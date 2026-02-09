5943c432218d8bc239df39ad265986ac268ab372
Ashish Bedi <ashishbedi02@gmail.com>
2026-02-09 15:25:13 +0530

    Fetch and Api's

---
+++ script2.js
@@ -0,0 +1,11 @@
+redBtn.addEventListener("click", () => {
+  document.body.style.backgroundColor = "red";
+});
+
+blueBtn.addEventListener("click", () => {
+  document.body.style.backgroundColor = "blue";
+});
+
+greenBtn.addEventListener("click", () => {
+  document.body.style.backgroundColor = "green";
+});
---
+++ script3.js
@@ -0,0 +1,35 @@
+// const btn = document.getElementById("btn");
+// const userList = document.getElementById("userList");
+
+// btn.addEventListener("click", () => {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//     .then(response => response.json())
+//     .then(payload => {
+//         userList.innerHTML = "";
+//         payload.forEach(user => {
+//             const li = document.createElement("li");
+//             li.innerHTML = `<strong>${user.name}</strong>`;
+//             userList.appendChild(li);
+//         })
+//     })
+// })
+
+
+const btn = document.getElementById("btn");
+const userList = document.getElementById("userList"); 
+
+btn.addEventListener("click", ()=> {
+fetch("https://jsonplaceholder.typicode.com/users")
+.then(Response => Response.json())
+.then(payload => {
+    userList.innerHTML = "";
+    payload.forEach(user => {
+        const li = document.createElement("li");
+        li.innerHTML = `${user.username} - ${user.email}`;
+        userList.appendChild(li);
+    })
+ })
+})
+
+
+//axios
