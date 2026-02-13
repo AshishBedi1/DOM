de5c379d1dc8245aa29fa426dfee2471157a9827
Ashish Bedi <ashish.bedi16042001@gmail.com>
2026-02-13 16:38:50 +0530

    pre increment post increment

--- script4.js
+++ script4.js
@@ -1,7 +1,22 @@
-let age = 61;
-if(age > 18 || age < 60){
-    console.log("You are eligible to work!");
-} 
-else{
-    console.log("You are not eligible to work!");
-}
+// let age = 61;
+// if(age > 18 || age < 60){
+//     console.log("You are eligible to work!");
+// } 
+// else{
+//     console.log("You are not eligible to work!");
+// }
+
+
+let a = 7;
+console.log(++a);  // 8
+
+let b = 7;
+console.log(b++); // 7
+console.log("test",b);   // 8
+
+let c = 10;
+let result =  c++ + 15;
+console.log(result);     //25
+let result2 = ++c + 15;
+console.log(result2);   //26
+
